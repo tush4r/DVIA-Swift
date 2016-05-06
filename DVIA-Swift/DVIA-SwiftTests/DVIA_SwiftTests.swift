6828a16f15f2fa0609c20c1ee8a36729148f08aa
tush4r <stushar52@gmail.com>
2016-05-06 23:52:41 +0530

    Initial files

---
+++ DVIA-Swift/DVIA-SwiftTests/DVIA_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  DVIA_SwiftTests.swift
+//  DVIA-SwiftTests
+//
+//  Created by Tushar Sharma on 06/05/16.
+//  Copyright © 2016 Edbinx. All rights reserved.
+//
+
+import XCTest
+@testable import DVIA_Swift
+
+class DVIA_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
